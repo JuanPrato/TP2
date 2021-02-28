1efa53d6afa05fc33024a8dd61b8b9b3e0583527
juankbn <jkarabin@fi.uba.ar>
2021-02-28 12:03:08 -0300

    agrego event handler vacio de bloque arriba

---
+++ src/main/java/edu/fiuba/algo3/controlador/BotonBloqueArribaEventHandler.java
@@ -0,0 +1,16 @@
+package edu.fiuba.algo3.controlador;
+
+import edu.fiuba.algo3.modelo.Tablero;
+import edu.fiuba.algo3.vista.ContenedorTablero;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BotonBloqueArribaEventHandler implements EventHandler {
+
+    @Override
+    public void handle(Event event) {
+        //aparecer en contenedorAlgoritmos
+    }
+}
