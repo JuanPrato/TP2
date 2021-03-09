2ce47798fc4606defd3d79a5cf4be9b981edea73
JuanPrato <jprato@fi.uba.ar>
2021-03-09 09:40:56 -0300

    maxPositions

--- src/main/java/edu/fiuba/algo3/modelo/direccion/Direccion.java
+++ src/main/java/edu/fiuba/algo3/modelo/direccion/Direccion.java
@@ -9,7 +9,8 @@ public class Direccion {
     private static Direccion abajo;
     private static Direccion derecha;
     private static Direccion izquierda;
-
+    private static int maxPositionX;
+    private static int maxPositionY;
 
     static {
         arriba = new Direccion(0, -1, "Arriba");
@@ -21,6 +22,9 @@ public class Direccion {
         abajo.setInversa(arriba);
         derecha.setInversa(izquierda);
         izquierda.setInversa(derecha);
+
+        maxPositionX = 110;
+        maxPositionY = 110;
 
     }
 
@@ -46,8 +50,9 @@ public class Direccion {
     public Posicion proximaPosicion(Posicion posicion) {
         // * 10 seria el espacio que ocuparia en pantalla la linea
         int nextX = posicion.getPosicionX() + this.valueX * 10;
+        if (nextX >= maxPositionX) nextX = maxPositionX;
         int nextY = posicion.getPosicionY() + this.valueY * 10;
-
+        if (nextX >= maxPositionY) nextY = maxPositionY;
         return new Posicion(nextX, nextY);
     }
 
